d92e832149900daaaf7901cce398b842b5b937d3
Mark van Beek <flipvb@gmail.com>
2018-03-27 17:21:33 +0200

    Fix manifest path

--- hooks/lib/android/manifestWriter.js
+++ hooks/lib/android/manifestWriter.js
@@ -2,6 +2,7 @@
 Class injects plugin preferences into AndroidManifest.xml file.
 */
 
+var fs = require('fs');
 var path = require('path');
 var xmlHelper = require('../xmlHelper.js');
 
@@ -19,6 +20,9 @@ module.exports = {
  */
 function writePreferences(cordovaContext, pluginPreferences) {
   var pathToManifest = path.join(cordovaContext.opts.projectRoot, 'platforms', 'android', 'AndroidManifest.xml');
+  if (!fs.existsSync(pathToManifest)) {
+    pathToManifest = path.join(cordovaContext.opts.projectRoot, 'platforms', 'android', 'app', 'src', 'main', 'AndroidManifest.xml');
+  }
   var manifestSource = xmlHelper.readXmlAsJson(pathToManifest);
   var cleanManifest;
   var updatedManifest;
